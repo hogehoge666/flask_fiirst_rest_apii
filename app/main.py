7a3263735cf16b332c9fa068640b35202171a29e
tnomura <hogehoge666@gmail.com>
2021-01-13 01:11:28 +0900

    added MySQL support for Docker environment

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
-from todoapi import app
+from todoapi import app, db
 from waitress import serve
 
 if __name__ == "__main__":
+    db.create_all()
     serve(app, host='0.0.0.0', port=5000)
--- app/todoapi/config.py
+++ app/todoapi/config.py
@@ -2,10 +2,10 @@ class Config():
     TODOAPI_CONFIG = "configured by Config in config.py"
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db/todoitems.db'
 
-
 class DevConfig(Config):
     TODOAPI_CONFIG = "configured by DevConfig in config.py"
 
 
 class DockerConfig(Config):
     TODOAPI_CONFIG = "configured by DockerConfig in config.py"
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysecret@todoapi-db/todoitems'
