650387f3ec8aa15275a8ada6fad5b42bc996a781
tnomura <hogehoge666@gmail.com>
2021-01-18 16:21:13 +0900

    waitress prints acccess logs to console

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from todoapi import app, db
 from waitress import serve
+from paste.translogger import TransLogger
+
+import logging
 
 if __name__ == "__main__":
     db.create_all()
+
+    app = TransLogger(app)
     serve(app, host='0.0.0.0', port=5000)
